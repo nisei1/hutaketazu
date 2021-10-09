65eabc1d62fa1cc4b6a1065a11b99807b0ffaa8b
laut0104 <78589722+laut0104@users.noreply.github.com>
2021-10-09 13:36:34 +0900

    likes revise
    revise

--- meetly/config/routes.rb
+++ meetly/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :users
   resources :posts do
-    resources :likes, only: [:create, :destroy]
+    resource :likes, only: [:create, :destroy]
   end
 end
