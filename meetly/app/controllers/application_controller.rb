3db66a8c288399010099b764f0d4ae0623e54ce3
yunapipi <supika0916yuna@icloud.com>
2021-10-09 17:38:27 +0900

    tabがよくわからない

--- meetly/app/controllers/application_controller.rb
+++ meetly/app/controllers/application_controller.rb
@@ -5,9 +5,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 
-  def after_sign_in_path_for(resource)
-    mypage_root_path # ログイン後に遷移するpathを設定
-  end
 
   def after_sign_out_path_for(resource)
     new_user_session_path # ログアウト後に遷移するpathを設定
