29bbf56e3c8ffa7b5512abe58a23dfc615a285fc
yunapipi <supika0916yuna@icloud.com>
2021-10-12 22:17:18 +0900

    Update home_controller.rb

--- meetly/app/controllers/home_controller.rb
+++ meetly/app/controllers/home_controller.rb
@@ -5,12 +5,14 @@ class HomeController < ApplicationController
   end
 
   def like
-    begin
-      current_user.likes.create(post_id: params[:post_id]) #user_idとpost_idの二つを代入
-      render json: {}, status: 200
-    rescue => e
-      p e
-      render json: {}, status: 500
-    end
+    # begin
+    #   current_user.likes.create(post_id: params[:post_id]) #user_idとpost_idの二つを代入
+    #   render json: {}, status: 200
+    # rescue => e
+    #   p e
+    #   render json: {}, status: 500
+    # end
+    current_user.likes.create(post_id: params[:post_id]) #user_idとpost_idの二つを代入
+    render json: {}, status: 200
   end
 end
