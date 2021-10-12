83a04c81cb91b457df58475bf61ec38543e70d2a
KNkym <43508107+nisei1@users.noreply.github.com>
2021-10-12 22:09:13 +0900

    dbとつなげて歌詞表示とLikeのリクエスト送信

--- meetly/app/controllers/home_controller.rb
+++ meetly/app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
+  protect_from_forgery except: :like  
   def index
+    @posts=Post.where().to_json
+  end
+
+  def like
+    begin
+      current_user.likes.create(post_id: params[:post_id]) #user_idとpost_idの二つを代入
+      render json: {}, status: 200
+    rescue => e
+      p e
+      render json: {}, status: 500
+    end
   end
 end
